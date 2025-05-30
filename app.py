55c5d886b6f2be4316577165479ae74bb24f815d
Grupo <vinicius.janini@aluno.faculdadeimpacta.com.br>
2025-05-30 19:44:33 -0300

    Ajustes finais

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from atividades.controller import atividades_blueprint
+from config import app
 
+app.register_blueprint(atividades_blueprint)
+
+if __name__ == "__main__":
+    app.run(
+        host=app.config["HOST"],
+        port=app.config["PORT"],
+        debug=app.config["DEBUG"]
+    )
+    
--- database/database.py
+++ database/database.py
@@ -1,20 +1,29 @@
-db = [
-    {
-        'id_atividade': 1,
-        'id_disciplina': 1,
-        'enunciado': 'Crie um app de todo em Flask',
-        'respostas': [
-            {'id_aluno': 1, 'resposta': 'todo.py', 'nota': 9},
-            {'id_aluno': 2, 'resposta': 'todo.zip.rar'},
-            {'id_aluno': 4, 'resposta': 'todo.zip', 'nota': 10}
-        ]
-    },
-    {
-        'id_atividade': 2,
-        'id_disciplina': 1,
-        'enunciado': 'Crie um servidor que envia email em Flask',
-        'respostas': [
-            {'id_aluno': 4, 'resposta': 'email.zip', 'nota': 10}
-        ]
-    }
-]
+from sqlalchemy import text
+from sqlalchemy.engine import create_engine
+
+engine = create_engine(url="postgresql://postgres:Az1310750412@127.0.0.1:5432/atividades", enable_from_linting=False, echo=False)
+ 
+class DatabaseManager:
+    
+    engine = engine
+ 
+    def _init_(self) -> None:
+        pass
+
+    def execute_sql_str(self, sql: str, query=False):
+        with self.engine.connect() as conn:
+            with conn.begin():
+                result = conn.execute(text(sql))
+                if query:
+                    return result.fetchall()
+                return None
+
+    def select_one(self, sql: str):
+        with self.engine.begin() as conn:
+            result = conn.execute(text(sql))
+            return result.fetchone()
+    
+    def select_all(self, sql: str):
+        with self.engine.begin() as conn:
+            result = conn.execute(text(sql))
+            return result.fetchall()
